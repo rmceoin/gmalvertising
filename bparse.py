566813624274dc1e64ca8fea7db16e34da37ab84
Randy McEoin <1653739+rmceoin@users.noreply.github.com>
2023-08-09 16:38:03 -0700

    added Bing

---
+++ bparse.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+#
+# bparse.py
+#
+# Parse an HTML file returned by Bing search
+# Report on any ads
+#
+
+import json
+import requests
+from bs4 import BeautifulSoup
+from os import path
+import re
+import sys
+
+ALLOWED_FILENAME = "allowed.txt"
+ALLOWED_CUSTOM_FILENAME = "allowed-custom.txt"
+MALICIOUS_FILENAME = "malicious.txt"
+MALICIOUS_CUSTOM_FILENAME = "malicious-custom.txt"
+
+ignore = []
+
+def read_list(filename: str) -> list:
+    data_list = []
+    if path.exists(filename):
+        file = open(filename, 'r')
+        data = file.read()
+        for line in data.split('\n'):
+            if line:
+                data_list.append(line)
+    return data_list
+
+ignore += read_list(ALLOWED_FILENAME)
+ignore += read_list(ALLOWED_CUSTOM_FILENAME)
+ignore += read_list(MALICIOUS_FILENAME)
+ignore += read_list(MALICIOUS_CUSTOM_FILENAME)
+
+ignore_list="|".join(ignore)
+re_ignore=r"("+ignore_list+")"
+#print(f're_ignore={re_ignore}')
+
+try:
+    filename = sys.argv[1]
+except IndexError:
+    raise SystemExit(f"Usage: {sys.argv[0]} <filename>")
+
+if path.exists(filename):
+    file = open(filename,mode='r')
+    html_text = file.read()
+    file.close()
+else:
+    print(f'Error: {filename} does not exist')
+    sys.exit(1)
+
+soup = BeautifulSoup(html_text, 'html.parser')
+
+regex = re.compile('.*sb_adTA.*')
+for div_ad in soup.find_all('div', {"class": "sb_adTA"}):
+#for div_ad in soup.find_all('div', {"class": regex}):
+#for sb_addesc in soup.find_all('div[class="sb_addesc"]'):
+#for sb_addesc in soup.find_all('p'):
+    #print(f'div_ad={div_ad}')
+    for atag in div_ad.find_all('a', {"class": "b_restorableLink"}):
+        #print(f'atag={atag}')
+        data_restore = atag.get('data-restore')
+        #print(f'data_restore={data_restore}')
+        data = json.loads(data_restore)
+        #print(f'data={data}')
+        displayUrl = data.get('displayUrl', None)
+        displayUrl = displayUrl.replace("<strong>","")
+        displayUrl = displayUrl.replace("</strong>","")
+        #print(f'displayUrl={displayUrl}')
+
+        ig = re.search(re_ignore, displayUrl)
+        #print(f'ig={ig}')
+        if not ig:
+            print(f'Found {displayUrl} {filename}')
--- gsearch.sh
+++ gsearch.sh
@@ -33,13 +33,36 @@ performquery () {
     fi
 
     output="$output_today/$query-$epoch.html"
-    #echo "Searching $query > $output"
+    echo "Searching $query > $output"
     http_code=$(curl "https://www.google.com/search?q=$query" -H 'referer: https://www.google.com/' \
       -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
       --compressed --output $output --silent --max-time 10 -w '%{http_code}')
     exit_status="$?"
     if [[ $http_code == "200" ]]; then
         ./gparse.py $output
+    else
+        echo "curl error:$http_code:$exit_status:$query:$output"
+    fi
+    sleep 1
+}
+
+performbingquery () {
+    query="$1"
+    today=$(date +%Y%m%d)
+    output_today="$outputdir/$today"
+    if [[ ! -e $output_today ]]; then
+        mkdir -p $output_today
+    fi
+
+    output="$output_today/bing-$query-$epoch.html"
+    echo "Searching $query > $output"
+    # https://www.bing.com/search?q=looksrare&qs=n&form=QBRE&sp=-1&lq=0&pq=looksrare&sc=15-9&sk=&cvid=F4468886830C498B9CB2B17F18956F22&ghsh=0&ghacc=0&ghpl=
+    http_code=$(curl "https://www.bing.com/search?q=$query" -H 'referer: https://www.bing.com/' \
+      -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+      --compressed --output $output --silent --max-time 10 -w '%{http_code}')
+    exit_status="$?"
+    if [[ $http_code == "200" ]]; then
+        ./bparse.py $output
     else
         echo "curl error:$http_code:$exit_status:$query:$output"
     fi
@@ -56,12 +79,14 @@ do
     for t in `cat queries.txt`; do
         rawurlencode "$t"
         performquery "$REPLY"
+        performbingquery "$REPLY"
     done
 
     if [[ -e "queries-custom.txt" ]]; then
         for t in `cat queries-custom.txt`; do
             rawurlencode "$t"
             performquery "$REPLY"
+            performbingquery "$REPLY"
         done
     fi
     sleep 60
