9ccd08a6b20e8f8e1c980b0003766be20297825e
Randy McEoin <1653739+rmceoin@users.noreply.github.com>
2024-04-26 13:08:32 -0500

    gsearch.py: comment out Accept-Encoding

--- gsearch.py
+++ gsearch.py
@@ -130,7 +130,7 @@ class GSearch:
             'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
             'referer': 'https://www.google.com/',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
+#            'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
             'Cache-Control': 'max-age=0',
             'Sec-Ch-Ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
